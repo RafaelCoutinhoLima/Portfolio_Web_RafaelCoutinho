5e189aa28583929ffa3d17961cf4fd61ceaff7bb
RafaelCoutinhoLima <rafaelcoutinholima12345@gmail.com>
2025-06-07 15:35:22 -0300

    ajeitando os botões

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.getElementById('voltar').addEventListener('click',function(){
-  window.location.href='index.html'
+document.getElementById("voltar").addEventListener('click',function(){
+  window.location.href="index.html"
 })
